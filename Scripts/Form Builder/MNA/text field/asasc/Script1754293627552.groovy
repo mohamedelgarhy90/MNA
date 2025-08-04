1b0c25f77e85dfd21529a856482ad400e621106e
iBlue <iBlue@Matar>
2025-08-04 16:38:24 +0300

    osamav1

--- Scripts/Form Builder/MNA/text field/asasc/Script1754293627552.groovy
+++ Scripts/Form Builder/MNA/text field/asasc/Script1754293627552.groovy
@@ -17,6 +17,6 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.callTestCase(findTestCase('Form Builder/MNA/1-Redirections/Navigate to plugin settings designer (Hossam) - Copy'), 
+WebUI.callTestCase(findTestCase('null'), 
     [:], FailureHandling.STOP_ON_FAILURE)
 
