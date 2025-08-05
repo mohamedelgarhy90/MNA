b4f87b4b636d20794b80dc2e53e35fd1fb2eea60
Penta <Penta@Penta-QC-Islam>
2025-08-05 15:31:03 +0300

    commit again 3shan islam ghaby

---
+++ Object Repository/Page_Home/a_Maps And Apps_rounded home_p-btn__ISRwu.rs
@@ -0,0 +1,146 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Maps And Apps_rounded home_p-btn__ISRwu</name>
+   <tag></tag>
+   <elementGuidId>ee76dcd3-873d-4b52-a7b1-01d09a92e39f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>a.rounded.home_p-btn__ISRwu</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='root']/div/div[2]/header/div/div/div/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Get Started&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>true</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>a4a8bc12-1445-4c53-880b-fb7ab0a64e6f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>rounded home_p-btn__ISRwu</value>
+      <webElementGuid>1dd5a208-37ab-4109-ae87-513f8638b103</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/penta-app-admin-entry-point-fb/MnA-fb/register/org</value>
+      <webElementGuid>47e4432e-e9fa-47c2-8242-b75d67db875b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Get Started</value>
+      <webElementGuid>d67d3051-90b0-4bec-a383-b108a393fec8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[@class=&quot;App d-flex flex-column&quot;]/div[@class=&quot;home_home__ebERQ&quot;]/header[1]/div[@class=&quot;container&quot;]/div[@class=&quot;col-12&quot;]/div[@class=&quot;home_header-content__oPhig&quot;]/a[@class=&quot;rounded home_p-btn__ISRwu&quot;]</value>
+      <webElementGuid>79c47ca6-f049-487e-b3ae-c0054c522a90</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/div/div[2]/header/div/div/div/a</value>
+      <webElementGuid>bb752341-3ad7-4fb2-9d96-cc5cb5b54e74</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Get Started')]</value>
+      <webElementGuid>c7a3ea7a-195c-484f-949b-6c5f2f0bc5a0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Maps And Apps'])[1]/following::a[1]</value>
+      <webElementGuid>9d9a0c2c-fb0c-4c28-84c2-5d5246e57d32</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='English'])[2]/following::a[1]</value>
+      <webElementGuid>9bc6810b-49ed-42eb-ba2e-cff10f3d553c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Organizations Types'])[1]/preceding::a[1]</value>
+      <webElementGuid>1c4841cb-f6fc-4758-8902-a715020e5c00</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Roles'])[1]/preceding::a[1]</value>
+      <webElementGuid>6c4ce511-8a5b-4978-8569-1053be47b98b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Get Started']/parent::*</value>
+      <webElementGuid>963f17b5-0166-4dd5-84fd-4aa5ba8f8c7d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/penta-app-admin-entry-point-fb/MnA-fb/register/org')]</value>
+      <webElementGuid>cb5e65fe-b8cc-4f02-b5a3-f3d11c4c1130</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//header/div/div/div/a</value>
+      <webElementGuid>b3322b7e-3b18-470d-b1bf-e93747cfe9b4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/penta-app-admin-entry-point-fb/MnA-fb/register/org' and (text() = 'Get Started' or . = 'Get Started')]</value>
+      <webElementGuid>4721568c-4c53-4986-b74b-daf1cf925344</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
